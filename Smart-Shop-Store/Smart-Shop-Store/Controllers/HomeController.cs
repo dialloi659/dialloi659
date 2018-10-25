ae0ccf60bb3afaba26d6db977376d5c8e8eae4b7
dialloi659 <44133766+dialloi659@users.noreply.github.com>
2018-10-25 10:36:57 +0200

    Bloc 3 DevWebApp
    Développement web et app dernière année Bac

---
+++ Smart-Shop-Store/Smart-Shop-Store/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Smart_Shop_Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ProductPageView()
+        {
+            LinkFile.LinkToSQLContextClassDataContext myContext = new LinkFile.LinkToSQLContextClassDataContext();
+
+            var DataDb = myContext.SelectAllProducts().ToList();
+            
+            return View(DataDb);
+        }
+    }
+}
